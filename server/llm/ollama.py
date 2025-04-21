bcd5989e9d4768e3485bf2e417fe69ef10aa4a61
Chris <chrisferg45@outlook.com>
2025-04-21 15:07:59 -0400

    Refactor Ollama class to encapsulate API details and improve summarize_text method with error handling and JSON response format.

--- server/llm/ollama.py
+++ server/llm/ollama.py
@@ -1,32 +1,45 @@
 import requests
-from os import getenv
 
-# Replace with your API key and endpoint
-url = getenv('ollama_url')
+class Ollama:
+    def __init__(self):
+        self.url = "http://localhost:11434/api/generate"
+        self.model = "gemma3:12b"
 
-def summarize_text(paragraph):
+    def summarize_text(self, paragraph):
+        """
+        This function will send a request to the Ollama API and return the summary of the text.
 
-    data = {
-        "model": "deepseek-r1",
-        "prompt": paragraph ,
-        # "text": paragraph,
-        "system": """
-        The user will provide text. The response should ingest the paragraph and provide an unbiased summary of the text. 
-        Begin each response with Unbiased Summary:""",
-        
-        "stream": False
-    }
+        Args:
+            paragraph (str): The text to summarize.
 
-    try:
-        response = requests.post(url, json=data)
-        
-        if response.status_code == 200:
-            result = response.json()
-            summary = result.get('response')
-            return summary
-        else:
-            print(f"Error: {response.status_code}, {response.text}")
+        Returns:
+            str: The summary of the text in JSON format.
+        """
+        data = {
+            "model": self.model,
+            "prompt": paragraph,
+            "format": "json",
+            # can add system prompt here using "system": """""",
+            "stream": False
+        }
+
+        try:
+            response = requests.post(self.url, json=data)
+            if response.status_code == 200:
+                result = response.json()
+                summary = result.get('response')
+                return summary
+            else:
+                print(f"Error: {response.status_code}, {response.text}")
+                return None
+        except Exception as e:
+            print(f'An error occured: {e}')
             return None
-    except Exception as e:
-        print(f'An error occured: {e}')
-        return None
+
+
+    
+if __name__ == "__main__":
+   ollama = Ollama()
+   result = ollama.summarize_text("Hello, how are you?")
+   print(result)
+   print(type(result))
