867d9b42211fd71c0e0d7e7adf6fb771262f232f
Chris <chrisferg45@outlook.com>
2025-04-21 16:01:10 -0400

    Core Ollama functionality class

--- server/llm/ollama.py
+++ server/llm/ollama.py
@@ -2,33 +2,83 @@ import requests
 
 class Ollama:
     def __init__(self):
-        self.url = "http://localhost:11434/api/generate"
+        self.url_generate = "http://localhost:11434/api/generate"
+        self.url_chat = "http://localhost:11434/api/chat"
         self.model = "gemma3:12b"
 
-    def summarize_text(self, paragraph):
+    def generate(self, message):
         """
-        This function will send a request to the Ollama API and return the summary of the text.
+        This function will send a request to the Ollama API and return the response as a JSON object. This should be used for
+        one-time generation tasks.
 
         Args:
-            paragraph (str): The text to summarize.
+            message (str): The users prompt.
 
         Returns:
-            str: The summary of the text in JSON format.
+            str: The response of the model in JSON format.
         """
         data = {
             "model": self.model,
-            "prompt": paragraph,
-            "format": "json",
-            # can add system prompt here using "system": """""",
+            "prompt": message,
             "stream": False
         }
 
         try:
-            response = requests.post(self.url, json=data)
+            response = requests.post(self.url_generate, json=data)
             if response.status_code == 200:
                 result = response.json()
-                summary = result.get('response')
-                return summary
+                model_response = result.get('response')
+                return model_response
+            else:
+                print(f"Error: {response.status_code}, {response.text}")
+                return None
+        except Exception as e:
+            print(f'An error occured: {e}')
+            return None
+    
+    def append_chat_history(self, messages, user_message, model_response):
+        """
+        This function adds user messages and their model responses to the chat history.
+
+        Args:
+            messages (list): A list of messages to send to the model.
+
+        Returns:
+            list: A list of messages to send to the model.
+        """
+        history = messages
+        history += [
+            {"role": "user", "content": user_message},
+            {"role": "assistant", "content": model_response},
+        ]
+        return history
+
+    def chat(self, messages, user_message):
+        """
+        This function will start a conversation with the model and return the response as a JSON object. This should be used for 
+        chatbot functionality that requires a conversation history.
+
+        Args:
+            messages (list): A list of messages to send to the model.
+        
+        Returns:
+            str: The response of the model in JSON format.
+        """
+        data = {
+            "model": self.model,
+            "messages": messages + [
+                {"role": "user", "content": user_message},
+                ],
+            "stream": False
+        }
+
+        try:
+            response = requests.post(self.url_chat, json=data)
+            if response.status_code == 200:
+                result = response.json()
+                model_response = result.get('message')
+                appended_chat_history = self.append_chat_history(messages, user_message, model_response['content'])
+                return model_response, appended_chat_history
             else:
                 print(f"Error: {response.status_code}, {response.text}")
                 return None
@@ -36,10 +86,18 @@ class Ollama:
             print(f'An error occured: {e}')
             return None
 
-
     
 if __name__ == "__main__":
+   # Test the generate function
+   print("Testing the generate function...")
    ollama = Ollama()
-   result = ollama.summarize_text("Hello, how are you?")
-   print(result)
-   print(type(result))
+   result = ollama.generate("Hello, how are you?")
+   print(f"Model response: {result}\n")
+   
+   # Test the chat function
+   print(f"Testing the chat function...")
+   messages = [{"role": "user", "content": "Is this working?"}, {"role": "assistant", "content": "Yes, it is working."}]
+   ollama = Ollama()
+   result = ollama.chat(messages, "Is this working?")
+   print(f"Model response: {result[0]}")
+   print(f"Chat history: {result[1]}")
